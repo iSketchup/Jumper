01c08b41443ba7992e26c3b6cdf6fb71143e0ea5
iSketchup <kalulu@gmx.at>
2025-12-19 00:09:01 +0100

    bug fix

--- docs/Game/script.js
+++ docs/Game/script.js
@@ -142,7 +142,7 @@ function loop() {
         if (score > highscore) {
             localStorage.setItem("highscore", score);
 }
-        window.location.href = "../index.html";
+        window.location.href = "/docs/index.html";
         return;
 
     }
